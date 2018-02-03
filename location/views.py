7c0dc4ee0ccd7401b207cd94d4e9a758dca39e27
arpit jain <arpitj938@gmail.com>
2018-02-03 13:47:50 +0530

    near location json done

--- location/views.py
+++ location/views.py
@@ -13,20 +13,22 @@ def near_location_json(request):
 	print latitude
 	print longitude
 	locations_rows = location_data.objects.all()
+	row = []
 	for o in locations_rows:
-		if((latitude + 0.01 == float(round(o.latittude,2))) or (latitude - 0.01 == float(round(o.latittude,2)))):
-			if((longitude + 0.01 == float(round(o.longitude,2))) or (longitude - 0.01 == float(round(o.longitude,2)))):
-				pass
+		if((latitude + 0.01 == float(round(float(o.lattitude),2))) or (latitude - 0.01 == float(round(float(o.lattitude),2)))):
+			if((longitude + 0.01 == float(round(float(o.longitude),2))) or (longitude - 0.01 == float(round(float(o.longitude),2)))):
+				row.append(o)
 	# rows = [o for o in locations_rows if  (((latitude + 0.01) == float(round(o.latitude, 2)) OR  ((latitude- 0.01) == float(round(o.latitude, 2)))) AND ((longitude+0.01) == float(round(o.longitude, 2)) OR (longitude- 0.01) == float(round(o.longitude, 2)))) ]
 	response_json = {}
+	response_json["data"] =[]
 	for o in row:
 		temp_json = {}
-		temp_json["name"] = str(o.loaction_name)
-		temp_json["address"] = str(o.loaction_address)
+		temp_json["name"] = str(o.location_name)
+		temp_json["address"] = str(o.location_address)
 		temp_json["m"] = o.m
 		temp_json["f"] = o.f
 		temp_json["hours"] = str(o.hours)
-		temp_json["latitude"] = o.latitude
+		temp_json["latitude"] = o.lattitude
 		temp_json["longitude"] = o.longitude
 		temp_json["overall"] = o.overall
 		response_json["data"].append(temp_json)
